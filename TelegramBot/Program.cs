59b22a49b48cad81af08234fcf2e636d2b9c9774
Дмитрий <@DESKTOP-IP3KOQF>
2021-03-23 17:07:38 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+        static void Main(string[] args)
+        {
+            string token = "";
+
+            bot = new TelegramBotClient(token);
+
+            bot.OnMessage += MesssageListener;
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        private static void MesssageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+            
+        }
+    }
+}
